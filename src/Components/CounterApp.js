481b07f1e6da46206984e49ee1a20aea524da3dd
David Pérez <deividpa022@gmail.com>
2022-11-19 00:40:15 -0500

    Se hace uso del Hook useState para alterar el valor del Counter por medio de React

--- src/Components/CounterApp.js
+++ src/Components/CounterApp.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(0);
+
+  const clickHandler = (e) => {
+    setCounter(counter + 1);
+  };
+
   return (
     <>
       <h1>Counter App</h1>
       <h2>{value}</h2>
+      <h3>Counter: {counter}</h3>
+      <br />
+      <button onClick={clickHandler}>Aumentar Contador</button>
     </>
   );
 };
