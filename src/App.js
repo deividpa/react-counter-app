aa77a7a56ac64bd82e2735221c5859a8ded6166d
David Pérez <deividpa022@gmail.com>
2022-11-18 23:53:12 -0500

    Se crea un react app a través de npx

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CounterApp from "./Components/CounterApp";
+
+function App() {
+  return (
+    <div className="App">
+      <CounterApp value={9585} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CounterApp.js
@@ -0,0 +1,10 @@
+const CounterApp = (value) => {
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>{value}</h2>
+    </>
+  );
+};
+
+export default CounterApp;
